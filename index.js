cbce8a066f866a576f6d1f98659cd4730146a3e8
Martin <101684319+Martind2082@users.noreply.github.com>
2024-01-07 17:17:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+let input = document.querySelector('input');
+let morsediv = document.querySelector('#morse');
+let correct = document.querySelector('#correct');
+
+let morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+alphabet = alphabet.split('');
+
+let word;
+let arr = [];
+async function grabmorse() {
+    arr = [];
+    fetch("https://random-word-api.herokuapp.com/word")
+    .then(res => res.json())
+    .then(data => {
+        word = data[0]
+    })
+    .then(() => {
+        for(let i = 0; i < word.length; i++) {
+            for (let j = 0; j < 26; j++) {
+                if (word[i] == alphabet[j]) {
+                    arr.push(`${morse[j]}/`)
+                    break;
+                }
+            }
+        }
+        arr = arr.join('');
+        morsediv.textContent = arr;
+    })
+}
+grabmorse()
+
+input.addEventListener("keydown", (e) => {
+    if (e.key == 'Enter') {
+        if (input.value.length == 0) {
+            return;
+        }
+        if (input.value == word) {
+            correct.style.display = 'block'; 
+            grabmorse();
+            input.value = "";
+            setTimeout(() => {
+                correct.style.display = 'none'; 
+            }, 1000);
+        } else {
+            wrong.style.display = 'block';
+            setTimeout(() => {
+                wrong.style.display = 'none'; 
+            }, 1000);
+        }
+    }
+})
